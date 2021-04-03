1585b96b93f640807014fda3b0e8ef015e1fd395
Dilum Aluthge <dilum@aluthge.com>
2021-04-02 23:18:40 -0400

    Add the `semver_spec_string` function (#3)

--- src/CompatEntryUtilities.jl
+++ src/CompatEntryUtilities.jl
@@ -1,5 +1,40 @@
 module CompatEntryUtilities
 
-# Write your package code here.
+import Pkg
 
+export semver_spec_string
+
+@static if Base.VERSION >= v"1.7-"
+    const VersionRange = Pkg.Versions.VersionRange
+    const VersionSpec = Pkg.Versions.VersionSpec
+else
+    const VersionRange = Pkg.Types.VersionRange
+    const VersionSpec = Pkg.Types.VersionSpec
 end
+
+function semver_spec_string(r::VersionRange)
+    m, n = r.lower.n, r.upper.n
+    if (m, n) == (0, 0)
+        return "≥0"
+    elseif m == 0
+        throw(ArgumentError("This version range cannot be represented using SemVer notation"))
+    elseif n == 0
+        return string("≥", join(r.lower.t, "."),)
+    else
+        return string(join(r.lower.t[1:m], "."), " - ", join(r.upper.t[1:n], "."))
+    end
+end
+
+"""
+    semver_spec_string(spec::Pkg.Versions.VersionSpec)
+
+Returns `str::AbstractString` such that `Pkg.Versions.semver_spec(str) == spec`.
+"""
+function semver_spec_string(spec::VersionSpec)
+    ranges = spec.ranges
+    isempty(ranges) && return "1 - 0"
+    specs = semver_spec_string.(ranges)
+    return join(specs, ", ")
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,96 @@
 using CompatEntryUtilities
 using Test
 
+import Pkg
+
+@static if Base.VERSION >= v"1.7-"
+    const PKG_VERSIONS = Pkg.Versions
+else
+    const PKG_VERSIONS = Pkg.Types
+end
+
+function roundtrip_semver_spec_string(str_1::AbstractString)
+    spec_1 = PKG_VERSIONS.semver_spec(str_1)
+    str_2 = semver_spec_string(spec_1)
+    spec_2 = PKG_VERSIONS.semver_spec(str_2)
+    str_3 = semver_spec_string(spec_2)
+    spec_3 = PKG_VERSIONS.semver_spec(str_3)
+    result = (spec_1 == spec_2) && (spec_1 == spec_3) && (spec_2 == spec_3)
+    if !result
+        @error("Roundtrip failed", str_1, str_2, str_3, spec_1, spec_2, spec_3)
+    end
+    return result
+end
+
 @testset "CompatEntryUtilities.jl" begin
-    # Write your tests here.
+    @testset "semver_spec_string" begin
+        @testset begin
+            let
+                lower = PKG_VERSIONS.VersionBound()
+                upper = PKG_VERSIONS.VersionBound(1)
+                r = PKG_VERSIONS.VersionRange(lower, upper)
+                spec = PKG_VERSIONS.VersionSpec([r])
+                msg = "This version range cannot be represented using SemVer notation"
+                @test_throws ArgumentError(msg) semver_spec_string(spec)
+            end
+        end
+
+        @testset begin
+            bases = ["0.0.3", "0.2.3", "1.2.3", "0.0", "0.2", "1.2", "0", "1"]
+            specifiers = ["", "^", "~", "= ", ">= ", "≥ "]
+            for specifier in specifiers
+                for base in bases
+                    @test roundtrip_semver_spec_string("$(specifier)$(base)")
+                end
+                @test roundtrip_semver_spec_string(join(string.(Ref(specifier), bases), ", "))
+            end
+        end
+
+        @testset begin
+            bases = ["0.0.3", "0.2.3", "1.2.3", "0.2", "1.2", "1"]
+            specifiers = ["< "]
+            for specifier in specifiers
+                for base in bases
+                    @test roundtrip_semver_spec_string("$(specifier)$(base)")
+                end
+                @test roundtrip_semver_spec_string(join(string.(Ref(specifier), bases), ", "))
+            end
+        end
+
+        @testset begin
+            strs = [
+                # ranges
+                "1.2.3 - 4.5.6",
+                "0.2.3 - 4.5.6",
+                "1.2 - 4.5.6",
+                "1 - 4.5.6",
+                "0.2 - 4.5.6",
+                "0.2 - 0.5.6",
+                "1.2.3 - 4.5",
+                "1.2.3 - 4",
+                "1.2 - 4.5",
+                "1.2 - 4",
+                "1 - 4.5",
+                "1 - 4",
+                "0.2.3 - 4.5",
+                "0.2.3 - 4",
+                "0.2 - 4.5",
+                "0.2 - 4",
+                "0.2 - 0.5",
+                "0.2 - 0",
+
+                # multiple ranges
+                "1 - 2.3, 4.5.6 - 7.8.9",
+
+                # other stuff
+                "1 - 0",
+                "2 - 1",
+                ">= 0",
+            ]
+
+            for str in strs
+                @test roundtrip_semver_spec_string(str)
+            end
+        end
+    end
 end
