fc788e7649d1112709868c23d01d7e2bdfa88e0b
Dilum Aluthge <dilum@aluthge.com>
2021-05-10 04:43:02 -0400

    Print some specifiers more concisely

--- src/CompatEntryUtilities.jl
+++ src/CompatEntryUtilities.jl
@@ -21,32 +21,46 @@ function _check_result(spec::PKG_VERSIONS.VersionSpec, str::String)
     return nothing
 end
 
-function _semver_spec_string(r::PKG_VERSIONS.VersionRange)
-    m, n = r.lower.n, r.upper.n
+function _semver_spec_string(r::PKG_VERSIONS.VersionRange)::String
+    m = r.lower.n
+    n = r.upper.n
+
     if (m, n) == (0, 0)
-        return "≥0"::String
-    elseif m == 0
-        msg = "This version range cannot be represented using SemVer notation"
-        @error(msg, r, r.lower, r.upper)
-        throw(ArgumentError(msg))
-    elseif n == 0
-        return string("≥", join(r.lower.t, "."),)::String
-    else
-        return string(join(r.lower.t[1:m], "."), " - ", join(r.upper.t[1:n], "."))::String
+        return "≥0"
     end
+
+    if ((m, n) == (3, 3)) && (r.lower == r.upper)
+        return string("=", join(r.lower.t, ".")) # "=x.y.z"
+    end
+
+    if ((m, n) == (3, 1)) && (r.lower.t[1] == r.upper.t[1]) && (r.lower.t[1] ≥ 1)
+        return join(r.lower.t, ".") # "x.y.z", which is equivalent to "^x.y.z"
+    end
+
+    if (m !=0 ) && (n == 0)
+        return string("≥", join(r.lower.t, "."))
+    end
+
+    if (m !=0 ) && (n != 0)
+        return string(join(r.lower.t[1:m], "."), " - ", join(r.upper.t[1:n], "."))
+    end
+
+    msg = "This version range cannot be represented using SemVer notation"
+    @error(msg, r, r.lower, r.upper)
+    throw(ArgumentError(msg))
 end
 
 """
-    semver_spec_string(spec::Pkg.Versions.VersionSpec)
+    semver_spec_string(spec::Pkg.Versions.VersionSpec)::String
 
 Returns `str::AbstractString` such that `Pkg.Versions.semver_spec(str) == spec`.
 """
 function semver_spec_string(spec::PKG_VERSIONS.VersionSpec)
     if isempty(spec.ranges)
-        result_string = "1 - 0"::String
+        result_string = "1 - 0"
     else
-        spec_strings = _semver_spec_string.(spec.ranges)::Vector{String}
-        result_string = join(spec_strings, ", ")::String
+        spec_strings = _semver_spec_string.(spec.ranges)
+        result_string = join(spec_strings, ", ")
     end
     _check_result(spec, result_string)
     return result_string
